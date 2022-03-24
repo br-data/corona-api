83c2edf54844b05224f76cf2c031d5a8c3ffb625
Steffen Kühne <stekhn@gmail.com>
2022-03-24 18:52:54 +0100

    Refactor code to use TypeScript #12

---
+++ src/downloaders/downloader_infektionen.ts
@@ -0,0 +1,340 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import { Downloader } from './downloader';
+import { fetch, getGithubFileMeta, csv2array, summarizer } from '../lib/helper';
+import { GenericObject } from '../lib/types';
+import { config } from '../lib/config';
+
+export class DownloaderInfektionen extends Downloader {
+  githubRepo = 'robert-koch-institut/SARS-CoV-2_Infektionen_in_Deutschland';
+  githubFile = 'Aktuell_Deutschland_SarsCov2_Infektionen.csv';
+
+  constructor() {
+    super('infektionen');
+  }
+
+  async checkUpdates() {
+    const file = await getGithubFileMeta(this.githubRepo, this.githubFile);
+
+    // Die Versionsnummer wird den Datei-Hashes angefügt.
+    // Wenn man sie erhöht, erzwingt man einen Datenupdate.
+    const hash = file.sha + '_' + config.version;
+
+    this.status.changed = this.status.hash !== hash;
+    this.status.newHash = hash;
+    this.status.lastCommitDate = file.lastCommitDate;
+
+    this.status.sources = {
+      infektionen: {
+        url: `https://media.githubusercontent.com/media/${this.githubRepo}/master/${this.githubFile}` // Git LFS
+      }
+    };
+  }
+
+  async doUpdate() {
+    console.log('      download');
+
+    const csv = await fetch(this.status.sources.infektionen.url);
+
+    fs.writeFileSync('test.csv', csv);
+
+    console.log('      process');
+
+    const data = csv2array(csv.toString(), ',', '\r\n');
+
+    const summaryLK = summarizer(
+      ['meldedatum', 'bundeslandId', 'landkreisId'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+    const summaryRB = summarizer(
+      ['meldedatum', 'bundeslandId', 'regierungsbezirk'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+    const summaryBL = summarizer(
+      ['meldedatum', 'bundeslandId'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+    const summaryDE = summarizer(
+      ['meldedatum'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+    const summaryLKNeu = summarizer(
+      ['bundeslandId', 'landkreisId'],
+      ['anzahlFallNeu', 'anzahlTodesfallNeu', 'anzahlGenesenNeu']
+    );
+    const summaryRBNeu = summarizer(
+      ['bundeslandId', 'regierungsbezirk'],
+      ['anzahlFallNeu', 'anzahlTodesfallNeu', 'anzahlGenesenNeu']
+    );
+    const summaryBLNeu = summarizer(
+      ['bundeslandId'],
+      ['anzahlFallNeu', 'anzahlTodesfallNeu', 'anzahlGenesenNeu']
+    );
+    const summaryDENeu = summarizer(
+      [],
+      ['anzahlFallNeu', 'anzahlTodesfallNeu', 'anzahlGenesenNeu']
+    );
+    const summaryBLAlt = summarizer(
+      ['meldedatum', 'bundeslandId', 'altersgruppe'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+    const summaryDEAlt = summarizer(
+      ['meldedatum', 'altersgruppe'],
+      ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
+    );
+
+    const regierungsbezirke = JSON.parse(
+      fs
+        .readFileSync(resolve(config.folders.static, 'regierungsbezirke.json'))
+        .toString()
+    );
+
+    let dateMin = '2020-01-01';
+    let dateMax = '2020-01-01'; // @TODO Is this correct?
+
+    data.forEach((e) => {
+      const landkreisId = parseInt(e.IdLandkreis, 10);
+      const anzahlFall = parseInt(e.AnzahlFall, 10);
+      const anzahlTodesfall = parseInt(e.AnzahlTodesfall, 10);
+      const anzahlGenesen = parseInt(e.AnzahlGenesen, 10);
+      const neuerFall = parseInt(e.NeuerFall, 10);
+      const neuerTodesfall = parseInt(e.NeuerTodesfall, 10);
+      const neuGenesen = parseInt(e.NeuGenesen, 10);
+
+      if (!e.IdLandkreis) {
+        console.log(e);
+      }
+
+      const entry = {
+        landkreisId: landkreisId,
+        bundeslandId: parseInt(e.IdLandkreis.slice(0, -3), 10),
+        regierungsbezirk: regierungsbezirke[landkreisId],
+        altersgruppe: cleanAltersgruppe(e.Altersgruppe),
+        //geschlecht: e.Geschlecht.toLowerCase(),
+        meldedatum: e.Meldedatum,
+        //refdatum: e.Refdatum,
+        //istErkrankungsbeginn: parseInt(e.IstErkrankungsbeginn, 10),
+        anzahlFall: neuerFall === 0 || neuerFall === 1 ? anzahlFall : 0,
+        anzahlTodesfall:
+          neuerTodesfall === 0 || neuerTodesfall === 1 ? anzahlTodesfall : 0,
+        anzahlGenesen: neuGenesen === 0 || neuGenesen === 1 ? anzahlGenesen : 0,
+        anzahlFallNeu: neuerFall === -1 || neuerFall === 1 ? anzahlFall : 0,
+        anzahlTodesfallNeu:
+          neuerTodesfall === -1 || neuerTodesfall === 1 ? anzahlTodesfall : 0,
+        anzahlGenesenNeu:
+          neuGenesen === -1 || neuGenesen === 1 ? anzahlGenesen : 0
+      };
+      if (dateMin > entry.meldedatum) dateMin = entry.meldedatum;
+      if (dateMax < entry.meldedatum) dateMax = entry.meldedatum;
+
+      // fasse Berlin zusammen
+      if (entry.landkreisId >= 11001 && entry.landkreisId <= 11012)
+        entry.landkreisId = 11000;
+
+      summaryLK.add(entry);
+      if (entry.regierungsbezirk) summaryRB.add(entry);
+      summaryBL.add(entry);
+      summaryDE.add(entry);
+
+      summaryLKNeu.add(entry);
+      if (entry.regierungsbezirk) summaryRBNeu.add(entry);
+      summaryBLNeu.add(entry);
+      summaryDENeu.add(entry);
+
+      summaryBLAlt.add(entry);
+      summaryDEAlt.add(entry);
+    });
+
+    console.log('      finalize');
+
+    let dataLK = summaryLK.get();
+    let dataRB = summaryRB.get();
+    let dataBL = summaryBL.get({ fillGaps: true });
+    let dataDE = summaryDE.get({ fillGaps: true });
+    let dataLKNeu = summaryLKNeu.get();
+    let dataRBNeu = summaryRBNeu.get();
+    let dataBLNeu = summaryBLNeu.get();
+    let dataDENeu = summaryDENeu.get();
+    let dataBLAlt = summaryBLAlt.get({ fillGaps: true });
+    let dataDEAlt = summaryDEAlt.get({ fillGaps: true });
+
+    console.log('      add meta data');
+
+    this.addMetadata(dataLK, ['bundeslaender', 'landkreise-einwohner']);
+    this.addMetadata(dataRB, ['bundeslaender', 'regierungsbezirke-einwohner']);
+    this.addMetadata(dataBL, ['bundeslaender-einwohner']);
+    this.addMetadata(dataDE, ['deutschland-einwohner']);
+    this.addMetadata(dataLKNeu, ['bundeslaender', 'landkreise-einwohner']);
+    this.addMetadata(dataRBNeu, [
+      'bundeslaender',
+      'regierungsbezirke-einwohner'
+    ]);
+    this.addMetadata(dataBLNeu, ['bundeslaender']);
+    this.addMetadata(dataDENeu, []);
+    this.addMetadata(dataBLAlt, ['bundeslaender-alter']);
+    this.addMetadata(dataDEAlt, ['deutschland-alter']);
+
+    console.log('      calculate incidences');
+
+    dataLK = calcInzidenzenUndSummen(dataLK, ['landkreisId']);
+    dataRB = calcInzidenzenUndSummen(dataRB, ['regierungsbezirk']);
+    dataBL = calcInzidenzenUndSummen(dataBL, ['bundeslandId']);
+    dataDE = calcInzidenzenUndSummen(dataDE);
+    dataBLAlt = calcInzidenzenUndSummen(dataBLAlt, [
+      'altersgruppe',
+      'bundeslandId'
+    ]);
+    dataDEAlt = calcInzidenzenUndSummen(dataDEAlt, ['altersgruppe']);
+    addLatestMeldedatum(dataLKNeu);
+    addLatestMeldedatum(dataRBNeu);
+    addLatestMeldedatum(dataBLNeu);
+    addLatestMeldedatum(dataDENeu);
+
+    console.log('      save');
+
+    this.saveTable('lk', dataLK);
+    this.saveTable('rb', dataRB);
+    this.saveTable('bl', dataBL);
+    this.saveTable('de', dataDE);
+    this.saveTable('lk-neu', dataLKNeu);
+    this.saveTable('rb-neu', dataRBNeu);
+    this.saveTable('bl-neu', dataBLNeu);
+    this.saveTable('de-neu', dataDENeu);
+    this.saveTable('bl-alt', dataBLAlt);
+    this.saveTable('de-alt', dataDEAlt);
+
+    function cleanAltersgruppe(text: string) {
+      switch (text) {
+        case 'unbekannt':
+          return 'unbekannt';
+        case 'A00-A04':
+          return '0-4';
+        case 'A05-A14':
+          return '5-14';
+        case 'A15-A34':
+          return '15-34';
+        case 'A35-A59':
+          return '35-59';
+        case 'A60-A79':
+          return '60-79';
+        case 'A80+':
+          return '80+';
+      }
+      throw Error('unbekannte Altersgruppe "' + text + '"');
+    }
+
+    function addLatestMeldedatum(list: GenericObject[]) {
+      list.forEach((e) => (e.meldedatum = dateMax));
+    }
+
+    function calcInzidenzenUndSummen(
+      data: GenericObject[],
+      groupKeys: string[] = []
+    ) {
+      const groups = new Map();
+      data.forEach((entry) => {
+        const key = groupKeys.map((k) => entry[k]).join('_');
+        if (!groups.has(key)) groups.set(key, []);
+        groups.get(key).push(entry);
+      });
+
+      const result: GenericObject[] = [];
+      Array.from(groups.values()).forEach((list) => {
+        // Vervollständige die Daten.
+        // Tage ohne Eintrag werden hinzugefügt, wobei die Werte alle Null sind.
+        const dateLookup = new Map(
+          list.map((e: GenericObject) => [e.meldedatum, e])
+        );
+        const obj0 = list[0];
+        forEachDate(dateMin, dateMax, (date: string) => {
+          if (dateLookup.has(date)) return;
+          let obj = Object.assign({}, obj0);
+          obj.meldedatum = date;
+          obj.anzahlFall = 0;
+          obj.anzahlTodesfall = 0;
+          obj.anzahlGenesen = 0;
+          list.push(obj);
+        });
+
+        list.sort((a: GenericObject, b: GenericObject) =>
+          a.meldedatum < b.meldedatum ? -1 : 1
+        );
+
+        // Berechne Inzidenzen
+        for (let i = 0; i < list.length; i++) {
+          let entry0 = list[i];
+          let minDatum = new Date(
+            Date.parse(entry0.meldedatum) - 6.1 * 84600000
+          )
+            .toISOString()
+            .slice(0, 10);
+          let j0 = Math.max(0, i - 6);
+          let sum = 0;
+          let count = 0;
+
+          for (let j = j0; j <= i; j++) {
+            let entry1 = list[j];
+            if (entry1.meldedatum < minDatum) continue;
+            sum += entry1.anzahlFall;
+            count++;
+          }
+          entry0.mittlere7TageInfektionen = Math.round((10 * sum) / 7) / 10;
+          entry0.inzidenz = Math.round((1e6 * sum) / entry0.einwohnerzahl) / 10;
+        }
+
+        // Berechne aggregierte Faelle
+        for (let i = 0; i < list.length; i++) {
+          let entry = list[i];
+          entry.summeFall = entry.anzahlFall;
+          entry.summeTodesfall = entry.anzahlTodesfall;
+          entry.summeGenesen = entry.anzahlGenesen;
+
+          if (i === 0) continue;
+
+          entry.summeFall += list[i - 1].summeFall;
+          entry.summeTodesfall += list[i - 1].summeTodesfall;
+          entry.summeGenesen += list[i - 1].summeGenesen;
+        }
+
+        // Berechne aktive Fälle und Genesene (Copyright: Niels-Magie)
+        list.map((d: GenericObject, i: number) => {
+          d.aktuellGenesen =
+            i < 14 ? 0 : list[i - 14].summeFall - d.summeTodesfall;
+          d.aktuellInfiziert =
+            d.summeFall - d.aktuellGenesen - d.summeTodesfall;
+        });
+
+        list.map((d: GenericObject, i: number) => {
+          const schwereFaelle = Math.floor(d.aktuellGenesen * 0.2);
+          d.aktuellGenesen -= schwereFaelle;
+          d.aktuellInfiziert += schwereFaelle;
+
+          for (let ii = 0; ii < schwereFaelle; ii++) {
+            const delay = Math.floor(Math.random() * 28) + 1;
+            if (i + delay < list.length) {
+              list[i + delay].aktuellGenesen += 1;
+              list[i + delay].aktuellInfiziert -= 1;
+            }
+          }
+        });
+
+        // Füge das Ergebnis hinzu
+        result.push(list);
+      });
+
+      return result.flat();
+    }
+
+    function forEachDate(
+      dateMin: string,
+      dateMax: string,
+      callback: (arg0: string) => void
+    ) {
+      let d0 = Math.floor(Date.parse(dateMin) / 86400000 + 0.5);
+      let d1 = Math.floor(Date.parse(dateMax) / 86400000 + 0.5);
+      for (let d = d0; d <= d1; d++) {
+        callback(new Date((d + 0.5) * 86400000).toISOString().slice(0, 10));
+      }
+    }
+  }
+}
---
+++ src/lib/helper.ts
@@ -0,0 +1,228 @@
+import https from 'https';
+import { GenericObject, GithubFile, GithubCommit } from './types';
+import { config } from './config';
+
+// Header für GitHub-API-Requests
+const gitHubAPIHeader = {
+  'User-Agent': 'curl/7.64.1',
+  Authorization:
+    'Basic ' + Buffer.from(config.githubAccessToken).toString('base64'),
+  'Content-Type': 'application/json;charset=UTF-8',
+  Accept: 'application/vnd.github.+json'
+};
+
+// @TODO Replace with node-fetch or a similar lib
+export async function fetch(url: string, headers = {}): Promise<string> {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, { headers }, (response) => {
+        const result: any[] = [];
+        response.on('data', (data) => result.push(data));
+        response.on('end', () => {
+          const buffer = Buffer.concat(result);
+          if (response.statusCode === 200) {
+            resolve(buffer.toString());
+          } else {
+            console.log('url:', url);
+            console.log('response:', response);
+            console.log(`Buffer: "${buffer.toString()}"`);
+            reject(buffer.toString());
+          }
+        });
+        response.on('error', (e) => {
+          console.log(e);
+          reject(e);
+        });
+      })
+      .on('error', (e) => {
+        console.log(e);
+        reject(e);
+      });
+  });
+}
+
+// @TODO Use Github client library instead
+export async function getGithubFileMeta(repo: string, filename: string) {
+  const filesRes = await fetch(
+    `https://api.github.com/repos/${repo}/contents`,
+    gitHubAPIHeader
+  );
+  const files = JSON.parse(filesRes) as GithubFile[];
+  const file = files.find((file) => file.name === filename);
+
+  if (!file) {
+    throw Error(
+      `Could not find "https://github.com/${repo}/blob/master/${filename}"`
+    );
+  }
+
+  const commitsRes = await fetch(
+    `https://api.github.com/repos/${repo}/commits?path=${filename}&per_page=1`,
+    gitHubAPIHeader
+  );
+  const commits = JSON.parse(commitsRes) as GithubCommit[];
+  file.lastCommit = commits[0];
+
+  if (file.lastCommit) {
+    file.lastCommitDate = file.lastCommit.commit.committer.date;
+  }
+
+  return file;
+}
+
+// Converts a CSV into an array of objects
+export function csv2array(
+  text: string,
+  fieldDelimiter = ',',
+  lineDelimiter = '\n'
+) {
+  const stripBom = (str: string) =>
+    str.charCodeAt(0) === 0xfeff ? str.slice(1) : str;
+  const data = stripBom(text)
+    .split(lineDelimiter)
+    .filter((line) => line.length > 0)
+    .map((line) => line.replace(/"|'/g, '').split(fieldDelimiter));
+  const keys = data.shift() as GenericObject;
+
+  return data.map((e) =>
+    Object.fromEntries(keys.map((k: string, i: number) => [k, e[i]]))
+  );
+}
+
+export function sortByKeys(data: GenericObject[], keys: string[]) {
+  // @ts-ignore
+  data.sort((a, b) => {
+    for (const key of keys) {
+      if (a[key] !== b[key]) {
+        return a[key] < b[key] ? -1 : 1;
+      }
+    }
+  });
+
+  return data;
+}
+
+// Checks, if the combination of keys is unique. Example:
+// { land:'bayern', date:'2022-01-01', … }
+// { land:'berlin', date:'2022-01-01', … }
+// { land:'bayern', date:'2022-01-02', … }
+// { land:'bayern', date:'2022-01-01', … } <--- combination of keys 'land' and 'date' is a duplicate
+export function checkUniqueKeys(data: GenericObject[], keys: string[]) {
+  const unique = new Set();
+  for (const entry of data) {
+    const key = keys.map((k) => entry[k]).join(';');
+    if (unique.has(key)) {
+      return false;
+    }
+    unique.add(key);
+  }
+  sortByKeys(data, keys);
+
+  return true;
+}
+
+export function summarizer(primaryKeys: string[], numericKeys: string[]) {
+  const map = new Map();
+
+  return { add, get };
+
+  function add(entry: GenericObject) {
+    let keyString = primaryKeys.map((k) => entry[k]).join(';');
+    if (!map.has(keyString)) {
+      let obj = {};
+      primaryKeys.forEach((k) => (obj[k] = entry[k]));
+      numericKeys.forEach((k) => (obj[k] = entry[k]));
+      map.set(keyString, obj);
+    } else {
+      let obj = map.get(keyString);
+      numericKeys.forEach((k) => (obj[k] += entry[k]));
+    }
+  }
+
+  function get(opt = { fillGaps: false }) {
+    // @TODO Check if this was working properly before
+    // if (opt.fillGaps) fillGaps(opt.fillGaps);
+    if (opt.fillGaps) {
+      fillGaps(primaryKeys);
+    }
+
+    const data = Array.from(map.values());
+    return sortByKeys(data, primaryKeys);
+  }
+
+  // Fügt nicht vorhandene Primay-Key-Kombinationen hinzu. Beispiel:
+  // {a:0,b:0,v:13},
+  // {a:0,b:1,v:14},
+  // {a:1,b:1,v:15},
+  // {a:1,b:2,v:16}
+  // Wären a und b die primaryKeys, dann wären für a die Werte [0,1] und b die Werte [0,1,2] definiert.
+  // Damit wären 2*3=6 Kombinationen möglich, aber nur 4 sind angegeben.
+  // Dementsprechend würde fillGaps die fehlenden Einträge hinzufügen und v als numericKey auf 0 setzen:
+  // {a:0,b:2,v:0},
+  // {a:1,b:0,v:0},
+
+  function fillGaps(keys: any[]) {
+    if (!Array.isArray(keys)) keys = primaryKeys;
+
+    if (keys.length < 2) return;
+
+    // scan keys
+    keys = keys.map((key) => ({ key, values: new Set() }));
+
+    // scan all known values for each key
+    for (let entry of map.values()) {
+      keys.forEach(({ key, values }) => values.add(entry[key]));
+    }
+
+    // generate all possible combinations of key values
+    let combinations = [] as GenericObject[];
+
+    keys.forEach(({ key, values }) => {
+      values = Array.from(values.values()) as string[];
+      values.sort((a: string, b: string) => (a < b ? -1 : 1));
+
+      if (!combinations.length) {
+        combinations = values.map((value: string) =>
+          Object.fromEntries([[key, value]])
+        ) as GenericObject[];
+      } else {
+        let newCombinations: GenericObject[] = [];
+        combinations.forEach((obj) => {
+          values.forEach((value: any) => {
+            obj = Object.assign({}, obj);
+            obj[key] = value;
+            newCombinations.push(obj);
+          });
+        });
+        combinations = newCombinations;
+      }
+    });
+
+    // add zeros for all sum fields
+    combinations.forEach((obj) => {
+      let keyString = primaryKeys.map((k) => obj[k]).join(';');
+      if (map.has(keyString)) return;
+      numericKeys.forEach((k) => (obj[k] = 0));
+      map.set(keyString, obj);
+    });
+  }
+}
+
+export function array2csv(list: GenericObject[]) {
+  const keys = Object.keys(
+    list.reduce((result, obj) => Object.assign(result, obj), {})
+  );
+  const csv = list.map((obj) =>
+    keys
+      .map((key) => {
+        let value = obj[key];
+        if (typeof value === 'string' && value.includes(',')) {
+          value = `"${value}"`;
+        }
+        return value;
+      })
+      .join(',')
+  );
+  csv.unshift(keys.join(','));
+  return csv.join('\n');
+}
