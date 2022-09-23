bf16745e7f86a800660dfeaa0e5db30e479d50b8
Constanze Bayer <39796792+looreen@users.noreply.github.com>
2022-09-23 16:56:55 +0200

    Aendern der geladenen Datei
    Aendern des Dateinamens und der gezogenen Spalten, Loeschen des Endpunkts Hospitalisierung nach Alter

--- src/downloaders/downloader_hospitalisierung.ts
+++ src/downloaders/downloader_hospitalisierung.ts
@@ -11,7 +11,7 @@ import { config } from '../lib/config';
 export class DownloaderHospitalisierungen extends Downloader {
   githubRepo =
     'robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland';
-  githubFile = 'Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
+  githubFile = 'Aktuell_Deutschland_adjustierte-COVID-19-Hospitalisierungen.csv';
 
   constructor() {
     super('hospitalisierung');
@@ -48,31 +48,32 @@ export class DownloaderHospitalisierungen extends Downloader {
         datum: e.Datum,
         bundeslandId: parseInt(e.Bundesland_Id, 10),
         altersgruppe: cleanAltersgruppe(e.Altersgruppe),
-        hospitalisierung7TFaelle: parseInt(e['7T_Hospitalisierung_Faelle'], 10),
-        hospitalisierung7TInzidenz: parseFloat(
-          e['7T_Hospitalisierung_Inzidenz']
-        )
+        hospitalisierung7TOhneNachmeldungFaelle: parseInt(e['fixierte_7T_Hospitalisierung_Faelle'], 10),
+        hospitalisierung7TMitNachmeldungFaelle: parseInt(e['aktualisierte_7T_Hospitalisierung_Faelle'], 10),
+        hospitalisierung7TSchaetzungFaelle: parseInt(e['PS_adjustierte_7T_Hospitalisierung_Faelle'], 10),
+        hospitalisierung7TObereSchaetzungFaelle: parseInt(e['OG_PI_adjustierte_7T_Hospitalisierung_Faelle'], 10),
+        hospitalisierung7TUntereSchaetzungFaelle: parseInt(e['UG_PI_adjustierte_7T_Hospitalisierung_Faelle'], 10),
+        hospitalisierung7TOhneNachmeldungInzidenz: parseFloat(e['fixierte_7T_Hospitalisierung_Inzidenz']),
+        hospitalisierung7TMitNachmeldungInzidenz: parseFloat(e['aktualisierte_7T_Hospitalisierung_Inzidenz']),
+        hospitalisierung7TSchaetzungInzidenz: parseFloat(e['PS_adjustierte_7T_Hospitalisierung_Inzidenz']),
+        hospitalisierung7TObereSchaetzungFaelleInzidenz: parseFloat(e['OG_PI_adjustierte_7T_Hospitalisierung_Inzidenz']),
+        hospitalisierung7TUntereSchaetzungInzidenz: parseFloat(e['UG_PI_adjustierte_7T_Hospitalisierung_Inzidenz'])
       };
       if (entry.bundeslandId > 0 && entry.altersgruppe === 'alle')
         dataBL.push(entry);
       if (entry.bundeslandId === 0 && entry.altersgruppe === 'alle')
         dataDE.push(entry);
-      if (entry.bundeslandId === 0 && entry.altersgruppe !== 'alle')
-        dataDEAlt.push(entry);
     });
 
     if (!checkUniqueKeys(dataBL, ['datum', 'bundeslandId'])) throw Error();
     if (!checkUniqueKeys(dataDE, ['datum'])) throw Error();
-    if (!checkUniqueKeys(dataDEAlt, ['datum', 'altersgruppe'])) throw Error();
 
     this.addMetadata(dataBL, ['bundeslaender']);
     this.addMetadata(dataDE, ['deutschland-einwohner']);
-    this.addMetadata(dataDEAlt, ['deutschland-alter']);
 
     this.saveTable('bl', dataBL);
     this.saveTable('de', dataDE);
-    this.saveTable('de-alter', dataDEAlt);
-
+    
     function cleanAltersgruppe(text: string) {
       switch (text) {
         case '00+':
