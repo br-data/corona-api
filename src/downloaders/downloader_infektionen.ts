3a21efc0583f5e8be535873f6659f31638759c25
Constanze Bayer <39796792+looreen@users.noreply.github.com>
2022-10-14 13:03:30 +0200

    Update downloader_infektionen.ts

--- src/downloaders/downloader_infektionen.ts
+++ src/downloaders/downloader_infektionen.ts
@@ -4,6 +4,8 @@ import { Downloader } from './downloader';
 import { fetch, getGithubFileMeta, csv2array, summarizer } from '../lib/helper';
 import { GenericObject } from '../lib/types';
 import { config } from '../lib/config';
+import regierungsbezirke from '../../static/regierungsbezirke.json';
+
 
 export class DownloaderInfektionen extends Downloader {
   githubRepo = 'robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland';
@@ -80,13 +82,13 @@ export class DownloaderInfektionen extends Downloader {
       ['meldedatum', 'altersgruppe'],
       ['anzahlFall', 'anzahlTodesfall', 'anzahlGenesen']
     );
-
+    /**
     const regierungsbezirke = JSON.parse(
       fs
         .readFileSync(resolve(config.folders.static, 'regierungsbezirke.json'))
         .toString()
     );
-
+     */
     let dateMin = '2020-01-01';
     let dateMax = '2020-01-01'; // @TODO Is this correct?
 
